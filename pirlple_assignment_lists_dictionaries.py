5410c4dc5cabf8e2675fbc06a4e4801188a843ab
Prabin Parajuli <prabin.b9@gmail.com>
2020-05-09 13:51:01 +0200

    Update pirlple_assignment_lists_dictionaries.py

--- pirlple_assignment_lists_dictionaries.py
+++ pirlple_assignment_lists_dictionaries.py
@@ -2,7 +2,13 @@
 # Refactor that code so all the variables are held as dictionary keys and value. 
 # Then refactor your print statements so that it's a single loop
 # that passes through each item in the dictionary and prints out it's key and then it's value.
-FavoriteSong = {"Song":"Wake Up", "Artist": "Arcade Fire", "Album": "funeral", "Year": 2004}
+FavoriteSong = {
+    "Song":"Wake Up", 
+    "Artist": "Arcade Fire", 
+    "Album": "funeral", 
+    "Year": "2004",
+    "Genre": "alternative"
+    }
 
 for key in FavoriteSong:
     print("{0:s}: \"{1:s}\"".format(key, FavoriteSong[key]))
