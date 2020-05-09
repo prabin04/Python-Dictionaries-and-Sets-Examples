30332b9949f0c9547c26d2fc0ea442df8795f9be
Prabin Parajuli <prabin.b9@gmail.com>
2020-05-09 12:54:13 +0200

    Add files via upload

---
+++ pirlple_assignment_lists_dictionaries.py
@@ -0,0 +1,8 @@
+# Return to your first homework assignments, when you described your favorite song.
+# Refactor that code so all the variables are held as dictionary keys and value. 
+# Then refactor your print statements so that it's a single loop
+# that passes through each item in the dictionary and prints out it's key and then it's value.
+FavoriteSong = {"Song":"Wake Up", "Artist": "Arcade Fire", "Album": "funeral", "Year": 2004}
+
+for key in FavoriteSong:
+    print("{0:s}: \"{1:s}\"".format(key, FavoriteSong[key]))
